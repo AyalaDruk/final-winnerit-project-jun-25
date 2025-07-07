5fd5266c70bb7b252a4a5ba0710f70fccbfc0202
AyalaDruk <ayalahorberg1999@gmail.com>
2025-07-07 23:32:43 +0300

    addind end to end test

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,3 +13,14 @@ def login_page_with_login(page: Page):
     lp.navigate()
     lp.expect_login_credentials_visible()
     return lp
+
+@pytest.fixture
+def logged_in_user(page: Page):
+    login_page = LoginPage(page)
+    login_page.navigate()
+    login_page.expect_login_credentials_visible()
+    login_page.type_username("standard_user")
+    login_page.fill_password("secret_sauce")
+    login_page.click_login_button()
+    login_page.expect_url_to_be("https://www.saucedemo.com/inventory.html")
+    return login_page
